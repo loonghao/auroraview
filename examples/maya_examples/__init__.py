3489f4ee5dbcf001748070a7a49f22b590f6129c
longhao <hal.long@outlook.com>
2025-11-08 22:10:40 +0800

    feat: add __init__.py to example packages and update import docs
    - Add __init__.py to all *_examples directories to make them proper Python packages
    - Update all README files with two import methods:
      - Method 1: import package.module as example (recommended)
      - Method 2: from package import module
    - This fixes ModuleNotFoundError when importing example packages
    
    Now users can use either:
      import nuke_examples.basic_panel as example
      OR
      from nuke_examples import basic_panel
    
    Signed-off-by: longhao <hal.long@outlook.com>

---
+++ examples/maya_examples/__init__.py
@@ -0,0 +1,10 @@
+"""AuroraView examples for Maya."""
+
+from pathlib import Path
+
+__version__ = "0.2.3"
+__all__ = ["basic_integration", "outliner_native", "qt_integration"]
+
+# Make the package directory available
+PACKAGE_DIR = Path(__file__).parent
+
